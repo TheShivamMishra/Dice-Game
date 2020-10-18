71fea92af9d75cb4addaf9763848b3360ef85523
Shivam Mishra <36327761+dudeshivam@users.noreply.github.com>
2020-10-18 16:05:10 +0530

    updated

--- script.js
+++ script.js
@@ -4,8 +4,15 @@ document.getElementsByTagName("button")[0].addEventListener("click",function(){
     console.log(p1 + " " + p2);
     var img1 = "./images/dice" + p1 + ".png";
     var img2 = "./images/dice" + p2 + ".png";
-    document.getElementById("img1").src = img1;
-    document.getElementById("img2").src = img2;
+    function anim(){
+        document.getElementById("img1").style.transform = "rotateY(180deg)";
+        document.getElementById("img1").style.transition = "all 1s";
+        document.getElementById("img2").style.transform = "rotateY(180deg)";
+        document.getElementById("img2").style.transition = "all 1s";
+        document.getElementById("img1").src = img1;
+        document.getElementById("img2").src = img2;
+    }
+    anim();
     if (p1 === p2) {
         document.querySelector(".container h1").textContent = "🚩 Match is draw 🚩";
     } else if (p1 > p2) {
