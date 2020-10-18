6abe093301e31dc31060a0625e6cdc959885c570
Shivam Mishra <36327761+dudeshivam@users.noreply.github.com>
2020-10-18 14:13:41 +0530

    update

---
+++ script.js
@@ -0,0 +1,20 @@
+var p1 = Math.floor(Math.random() * 6) + 1;
+var p2 = Math.floor(Math.random() * 6) + 1;
+console.log(p1 + " "+ p2);
+var img1 = "./images/dice"+ p1+".png";
+var img2 = "./images/dice" + p2+".png";
+document.getElementById("img1").src = img1;
+document.getElementById("img2").src = img2;
+if(p1===p2)
+{
+    document.querySelector(".container h1").textContent = "🚩 Match is draw 🚩";
+}else if(p1>p2)
+{
+    document.querySelector(".container h1").textContent = "🚩 Player 1 is winner!";
+}else{
+    document.querySelector(".container h1").textContent = "Player 2 is winner! 🚩";
+}
+
+document.getElementsByTagName("button")[0].addEventListener("click",function(){
+    window.location.reload();
+})
